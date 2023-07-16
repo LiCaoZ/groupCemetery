f25d3dbb5ce84dcb3433852d1a763719ac9e968d
LiCaoZ <admin@licaoz.com>
2023-07-16 14:27:00 +0800

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ xhr.onreadystatechange = function () {
         console.log(data);
         var dataLength = data.length;
         // 解析并渲染数据
-        detailDOM = document.querySelector(".detail");
+        detailDOM = document.getElementsByClassName("detail");
 
         for (var i = 0; i < dataLength; i++) {
             // 拼接 HTML
@@ -25,9 +25,9 @@ xhr.onreadystatechange = function () {
             var suicide_note_url = data[i].suicide_note_url;
             var html = '<div class="mdui-card mdui-col-xs-12 mdui-col-sm-4"> <div class="mdui-card-header"> <img class="mdui-card-header-avatar" src=' + avatar + ' /> <div class="mdui-card-header-title">' + Name + '</div> <div class="mdui-card-header-subtitle">' + Age + '</div> </div> <div class="mdui-card-content"> <p class="date">在 <span class="date">' + date + '</span> 永远离开了我们</p> </div> <div class="mdui-card-actions"> <a class="mdui-btn mdui-ripple suicide-note" href=" '+ suicide_note_url + '">查阅遗书</a> <button class="mdui-btn mdui-ripple">上一柱香</button> <button class="mdui-btn mdui-float-right">更多</button> </div> </div>';
             // Remove mdui-progress
-            document.querySelector(".mdui-progress").remove();
+            document.getElementsByClassName("mdui-progress").remove();
             // 插入 HTML
-            detailDOM.append(html);
+            detailDOM.insertAdjacentHTML('beforeend', html) ;
             if (suicide_note_url == null) {
                 // 禁用 .suicide-note
                 document.getElementsByClassName('suicide-note').setAttribute('disabled')
